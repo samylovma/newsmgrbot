21203e186cf5964df5dd664165909f0e4667da01
Mikhail Samylov <134856751+samylovma@users.noreply.github.com>
2024-08-27 15:33:44 +0300

    chore: add `alembic`

--- src/newsmgrbot/models/__init__.py
+++ src/newsmgrbot/models/__init__.py
@@ -1,11 +1,17 @@
 __all__ = (
+    "registry",
     "News",
     "Source",
     "User",
     "user_source",
 )
 
+import sqlalchemy.orm
+from advanced_alchemy.base import orm_registry
+
 from ._news import News
 from ._source import Source
 from ._user import User
 from ._user_source import user_source
+
+registry: sqlalchemy.orm.registry = orm_registry
