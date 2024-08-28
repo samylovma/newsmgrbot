995f05747f77154d618b82c62c8dc33846ab80cc
Mikhail Samylov <134856751+samylovma@users.noreply.github.com>
2024-08-28 14:47:21 +0300

    refactor: simplify parser, add protection from exceptions while scraping

--- src/newsmgrbot/callbacks/parser.py
+++ src/newsmgrbot/callbacks/parser.py
@@ -1,5 +1,7 @@
 import asyncio
-from typing import TYPE_CHECKING
+import logging
+from datetime import UTC, datetime
+from typing import Any
 
 from dishka import FromDishka
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup
@@ -10,8 +12,7 @@ from newsmgrbot.services.news import NewsService
 from newsmgrbot.services.scraper import FeedScraper
 from newsmgrbot.services.source import SourceService
 
-if TYPE_CHECKING:
-    from newsmgrbot.models import News
+_logger = logging.getLogger(__name__)
 
 
 @inject
@@ -21,40 +22,38 @@ async def parser_callback(
     news_service: FromDishka[NewsService],
     feed_scraper: FromDishka[FeedScraper],
 ) -> None:
-    new_news: list[News] = []
+    news_data_list: list[dict[str, Any]] = []
     sources = await source_service.list()
-    feeds = await asyncio.gather(*[feed_scraper.scrap(source.feed_url) for source in sources])
+    feeds = await asyncio.gather(*[feed_scraper.scrap(source.feed_url) for source in sources], return_exceptions=True)
     for source, feed in zip(sources, feeds, strict=True):
-        for news in feed.news:
-            is_exists = await news_service.exists(source_id=source.id, internal_id=news.id)
-            if not is_exists and news.pub_date >= source.created_at:
-                db_news = await news_service.create(
+        if isinstance(feed, BaseException):
+            _logger.error("Ignoring the exception while scraping the source with ID %s.", source.id, exc_info=feed)
+            continue
+        for feed_news in feed.news:
+            if (
+                feed_news.pub_date >= source.created_at
+                and feed_news.pub_date <= datetime.now(UTC)
+                and not await news_service.exists(source_id=source.id, internal_id=feed_news.id)
+            ):
+                news_data_list.append(  # noqa: PERF401
                     {
                         "source_id": source.id,
-                        "internal_id": news.id,
-                        "title": news.title,
-                        "url": news.url,
-                        "pub_date": news.pub_date,
-                        "description": news.description,
+                        "internal_id": feed_news.id,
+                        "title": feed_news.title,
+                        "url": feed_news.url,
+                        "pub_date": feed_news.pub_date,
+                        "description": feed_news.description,
                     }
                 )
-                new_news.append(db_news)
-
-    for i in range(len(new_news)):
-        for j in range(i + 1, len(new_news)):
-            if new_news[i].pub_date > new_news[j].pub_date:
-                new_news[i], new_news[j] = new_news[j], new_news[i]
-
-    for news in new_news:
-        source = await source_service.get(news.source_id)
-        text = f"<b>«{news.title}» — «{source.title}»</b>"
+    news_list = await news_service.create_many(news_data_list)
+    news_list = sorted(news_list, key=lambda news: news.pub_date)
+    for news in news_list:
+        text = f"<b>{news.title} — {news.source.title}</b>"
         if news.description:
             text += f"\n\n<blockquote expandable>{news.description}</blockquote>"
-        for user_id in source.users:
-            context.application.create_task(
-                context.bot.send_message(
-                    chat_id=user_id,
-                    text=text,
-                    reply_markup=InlineKeyboardMarkup.from_button(InlineKeyboardButton(text="Link", url=news.url)),
-                )
+        for user in news.source.users:
+            await context.bot.send_message(
+                chat_id=user.tg_id,
+                text=text,
+                reply_markup=InlineKeyboardMarkup.from_button(InlineKeyboardButton(text="Link", url=news.url)),
             )
