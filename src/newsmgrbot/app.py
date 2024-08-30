bb9b70463f3c805e33c33726b378b95f8241aefc
Mikhail Samylov <134856751+samylovma@users.noreply.github.com>
2024-08-30 18:16:39 +0300

    chore: improve app fabric

--- src/newsmgrbot/app.py
+++ src/newsmgrbot/app.py
@@ -1,15 +1,19 @@
 import datetime
+from typing import Any, cast
 
 import dishka
-from telegram import LinkPreviewOptions, Update
+from telegram import BotCommand, LinkPreviewOptions, Update
 from telegram.constants import ParseMode
 from telegram.ext import (
     AIORateLimiter,
     Application,
+    CallbackContext,
     CallbackQueryHandler,
     CommandHandler,
     ConversationHandler,
     Defaults,
+    ExtBot,
+    JobQueue,
     MessageHandler,
     TypeHandler,
     filters,
@@ -29,40 +33,72 @@ from newsmgrbot.callbacks.start import start_callback
 from newsmgrbot.config import Config
 from newsmgrbot.provider import Provider
 
+type _Application = Application[
+    ExtBot[AIORateLimiter],
+    CallbackContext[ExtBot[AIORateLimiter], dict[Any, Any], dict[Any, Any], dict[Any, Any]],
+    dict[Any, Any],
+    dict[Any, Any],
+    dict[Any, Any],
+    JobQueue[CallbackContext[ExtBot[AIORateLimiter], dict[Any, Any], dict[Any, Any], dict[Any, Any]]],
+]
 
-def create_app(config: Config) -> Application:
-    application = (
-        Application.builder()
-        .token(config.TELEGRAM_BOT_TOKEN)
-        .defaults(
-            Defaults(
-                parse_mode=ParseMode.HTML,
-                link_preview_options=LinkPreviewOptions(is_disabled=True),
+
+def create_app(config: Config) -> _Application:
+    app = cast(
+        _Application,
+        (
+            Application.builder()
+            .token(config.TELEGRAM_BOT_TOKEN)
+            .defaults(
+                Defaults(
+                    parse_mode=ParseMode.HTML,
+                    link_preview_options=LinkPreviewOptions(is_disabled=True),
+                )
             )
-        )
-        .rate_limiter(AIORateLimiter())
-        .build()
+            .post_init(_post_init)
+            .post_shutdown(_post_shutdown)
+            .rate_limiter(AIORateLimiter())
+            .build()
+        ),
     )
-    application.add_handlers(
+    app.add_handlers(
         {
             -1: [TypeHandler(Update, auth_callback)],
             0: [
-                CommandHandler("start", start_callback),
-                CommandHandler("help", help_callback),
-                CommandHandler("privacy", privacy_callback),
+                CommandHandler("start", start_callback),  # type: ignore[arg-type]
+                CommandHandler("help", help_callback),  # type: ignore[arg-type]
+                CommandHandler("privacy", privacy_callback),  # type: ignore[arg-type]
                 CommandHandler("sources", sources_callback),
                 CallbackQueryHandler(check_source_callback, r"^source_"),
                 ConversationHandler(
-                    entry_points=[CallbackQueryHandler(new_source_entry, r"^new_source$")],
+                    entry_points=[
+                        CallbackQueryHandler(new_source_entry, r"^new_source$"),  # type: ignore[arg-type]
+                    ],
                     states={1: [MessageHandler(filters.TEXT, new_source_feed_url)]},
                     fallbacks=[],
                 ),
             ],
         }
     )
-    application.job_queue.run_repeating(
+    app.job_queue.run_repeating(  # type: ignore[union-attr]
         callback=parser_callback,
         interval=datetime.timedelta(minutes=1),
     )
-    application.bot_data["container"] = dishka.make_async_container(Provider(db_url=config.DATABASE_URL))
-    return application
+    app.bot_data["container"] = dishka.make_async_container(Provider(db_url=config.DATABASE_URL))
+    return app
+
+
+async def _post_init(application: _Application) -> None:
+    await application.bot.set_my_commands(
+        commands=(
+            BotCommand(command="start", description="introduction message"),
+            BotCommand(command="help", description="help message"),
+            BotCommand(command="privacy", description="privacy policy"),
+            BotCommand(command="sources", description="manage your sources"),
+        )
+    )
+
+
+async def _post_shutdown(application: _Application) -> None:
+    container: dishka.AsyncContainer = application.bot_data["container"]
+    await container.close()
