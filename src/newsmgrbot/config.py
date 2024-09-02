1bfa7978daa948553d8fb3029c6127280ccf22dd
Mikhail Samylov <134856751+samylovma@users.noreply.github.com>
2024-09-02 12:56:49 +0300

    feat: add user agent customization for scraper

--- src/newsmgrbot/config.py
+++ src/newsmgrbot/config.py
@@ -9,6 +9,7 @@ class Config:
     TELEGRAM_BOT_TOKEN: str
     DATABASE_URL: str
     SCRAPER_PROXY: str | None = None
+    SCRAPER_USER_AGENT: str | None = None
 
 
 def parse_config() -> Config:
@@ -17,4 +18,5 @@ def parse_config() -> Config:
         TELEGRAM_BOT_TOKEN=os.environ["TELEGRAM_BOT_TOKEN"],
         DATABASE_URL=os.environ["DATABASE_URL"],
         SCRAPER_PROXY=os.getenv("SCRAPER_PROXY"),
+        SCRAPER_USER_AGENT=os.getenv("SCRAPER_USER_AGENT"),
     )
--- src/newsmgrbot/provider.py
+++ src/newsmgrbot/provider.py
@@ -4,6 +4,7 @@ import dishka
 import httpx
 import sqlalchemy.ext.asyncio as sa
 
+from newsmgrbot.config import Config
 from newsmgrbot.services.news import NewsService
 from newsmgrbot.services.scraper import FeedScraper
 from newsmgrbot.services.source import SourceService
@@ -11,14 +12,13 @@ from newsmgrbot.services.user import UserService
 
 
 class Provider(dishka.Provider):
-    def __init__(self, db_url: str, scraper_proxy: str | None) -> None:
+    def __init__(self, config: Config) -> None:
         super().__init__()
-        self.__db_url: str = db_url
-        self.__scraper_proxy: str | None = scraper_proxy
+        self.__config: Config = config
 
     @dishka.provide(scope=dishka.Scope.APP)
     async def provide_sa_engine(self) -> sa.AsyncEngine:
-        return sa.create_async_engine(self.__db_url)
+        return sa.create_async_engine(self.__config.DATABASE_URL)
 
     @dishka.provide(scope=dishka.Scope.APP)
     async def provide_sa_sessionmaker(self, sa_engine: sa.AsyncEngine) -> sa.async_sessionmaker[sa.AsyncSession]:
@@ -26,7 +26,10 @@ class Provider(dishka.Provider):
 
     @dishka.provide(scope=dishka.Scope.APP)
     async def provide_feed_scraper(self) -> AsyncIterable[FeedScraper]:
-        async with httpx.AsyncClient(proxy=self.__scraper_proxy) as client:
+        async with httpx.AsyncClient(
+            headers={"user-agent": self.__config.SCRAPER_USER_AGENT},
+            proxy=self.__config.SCRAPER_PROXY,
+        ) as client:
             yield FeedScraper(client=client)
 
     @dishka.provide(scope=dishka.Scope.REQUEST)
